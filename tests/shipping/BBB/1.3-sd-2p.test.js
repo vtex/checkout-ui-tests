4aff5fb126e91b26c25929ef728986eca3d020e1
nandoacoelho <nandoacoelho@gmail.com>
2019-04-10 01:21:47 -0300

    Update e2e tests

---
+++ tests/shipping/BBB/1.3-sd-2p.test.js
@@ -0,0 +1,34 @@
+import { setup, visitAndClearCookies, deleteAllCookies } from "../../../utils";
+import {
+  fillEmail,
+  getSecondPurchaseEmail,
+  confirmSecondPurchase
+} from "../../../utils/profile-actions";
+import {
+  payWithPaymentSlip,
+  completePurchase
+} from "../../../utils/payment-actions";
+
+xdescribe("Scheduled Delivery - 2P", () => {
+  before(() => {
+    visitAndClearCookies();
+    deleteAllCookies();
+  });
+
+  it("start with delivery then, choosing pickup, then choosing delivery", () => {
+    const email = getSecondPurchaseEmail();
+
+    setup({ skus: ["299"] });
+    fillEmail(email);
+    confirmSecondPurchase();
+    payWithPaymentSlip();
+    completePurchase();
+
+    cy.url({ timeout: 30000 }).should("contain", "/orderPlaced");
+    cy.contains(email).should("be.visible");
+    cy.contains("Gab**** God**").should("be.visible");
+    cy.contains("Receber").should("be.visible");
+    cy.contains("Bot*****").should("be.visible");
+  });
+});
+// OK
