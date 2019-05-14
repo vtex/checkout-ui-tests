930e11e2fe790b1154082d295102a63a02d41c0a
nandoacoelho <nandoacoelho@gmail.com>
2019-05-14 11:16:55 -0300

    Update end2end tests

--- tests/shipping/BBB/1.2-Pickup - Second Purchase - Boleto.test.js
+++ tests/shipping/BBB/1.2-Pickup - Second Purchase - Boleto.test.js
@@ -1,33 +1,39 @@
-import { setup, visitAndClearCookies, deleteAllCookies } from "../../../utils";
+
+import { setup, visitAndClearCookies } from "../../../utils";
 import {
   fillEmail,
   getSecondPurchaseEmail,
   confirmSecondPurchase
 } from "../../../utils/profile-actions";
 import {
+  choosePickupOmnishipping,
+  goToPayment,
+  chooseDeliveryOmnishipping
+} from "../../../utils/shipping-actions";
+import {
   payWithPaymentSlip,
   completePurchase
 } from "../../../utils/payment-actions";
 
-describe("Scheduled Delivery - 2P", () => {
+describe("Pickup - 2P", () => {
   before(() => {
     visitAndClearCookies();
-    deleteAllCookies();
   });
 
-  it("start with delivery then, choosing pickup, then choosing delivery", () => {
+  it("start with delivery then, choosing pickup, then choosing pickup", () => {
     const email = getSecondPurchaseEmail();
 
-    setup({ skus: ["299"] });
+    setup({ skus: ["285"] });
     fillEmail(email);
     confirmSecondPurchase();
+    goToPayment();
     payWithPaymentSlip();
     completePurchase();
 
     cy.url({ timeout: 30000 }).should("contain", "/orderPlaced");
     cy.contains(email).should("be.visible");
     cy.contains("Gab**** God**").should("be.visible");
-    cy.contains("Receber").should("be.visible");
-    cy.contains("Bot*****").should("be.visible");
+    cy.contains("Loja em Copacabana no Rio de Janeiro").should("be.visible");
+    cy.contains("Retirar").should("be.visible");
   });
 });
--- tests/shipping/BBB/1.3-Scheduled Delivery - Second Purchase - Boleto.test.js
+++ tests/shipping/BBB/1.3-Scheduled Delivery - Second Purchase - Boleto.test.js
@@ -1,38 +1,33 @@
-import { setup, visitAndClearCookies } from "../../../utils";
+import { setup, visitAndClearCookies, deleteAllCookies } from "../../../utils";
 import {
   fillEmail,
   getSecondPurchaseEmail,
   confirmSecondPurchase
 } from "../../../utils/profile-actions";
 import {
-  choosePickupOmnishipping,
-  goToPayment,
-  chooseDeliveryOmnishipping
-} from "../../../utils/shipping-actions";
-import {
   payWithPaymentSlip,
   completePurchase
 } from "../../../utils/payment-actions";
 
-describe("Pickup - 2P", () => {
+xdescribe("Scheduled Delivery - 2P", () => {
   before(() => {
     visitAndClearCookies();
+    deleteAllCookies();
   });
 
-  it("start with delivery then, choosing pickup, then choosing pickup", () => {
+  it("start with delivery then, choosing pickup, then choosing delivery", () => {
     const email = getSecondPurchaseEmail();
 
-    setup({ skus: ["285"] });
+    setup({ skus: ["299"] });
     fillEmail(email);
     confirmSecondPurchase();
-    goToPayment();
     payWithPaymentSlip();
     completePurchase();
 
     cy.url({ timeout: 30000 }).should("contain", "/orderPlaced");
     cy.contains(email).should("be.visible");
     cy.contains("Gab**** God**").should("be.visible");
-    cy.contains("Loja em Copacabana no Rio de Janeiro").should("be.visible");
-    cy.contains("Retirar").should("be.visible");
+    cy.contains("Receber").should("be.visible");
+    cy.contains("Bot*****").should("be.visible");
   });
 });
