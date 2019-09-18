a905e0daddddebc5d4d2cbd74046e02388a16800
nandoacoelho <nandoacoelho@gmail.com>
2019-09-18 11:18:51 -0300

    Update payment actions to re-query iframe

--- utils/payment-actions.js
+++ utils/payment-actions.js
@@ -6,65 +6,85 @@ export function payWithPaymentSlip() {
   cy.get("#payment-group-bankInvoicePaymentGroup").click({ force: true })
 }
 
+function getIframeBody($iframe) {
+  return $iframe.contents().find("body")
+}
+
+function queryIframe(callback) {
+  cy.get("iframe").then(callback)
+}
+
 export function payWithCreditCard(options = { withAddress: false }) {
   cy.wait(5000)
-  cy.get("iframe").then($iframe => {
-    const $body = $iframe.contents().find("body")
-    cy.wait(1000)
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Number")
       .type("4040240009008936")
-    cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Name")
       .type("Fernando A Coelho")
     cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Brand")
       .select("1")
     cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Month")
       .select("02")
     cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Year")
       .select("22")
     cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Code")
       .type("066")
     cy.wait(1000)
+  })
 
-    if (options.withAddress) {
-      cy.wrap($body)
+  if (options.withAddress) {
+    queryIframe($iframe => {
+      cy.wrap(getIframeBody($iframe))
         .find("#payment-billing-address-postalCode-0")
         .type("22071060")
+    })
 
-      cy.wrap($body)
+    queryIframe($iframe => {
+      cy.wrap(getIframeBody($iframe))
         .find("#payment-billing-address-number-0")
         .type("12")
-    }
-  })
+    })
+  }
 }
 
 export function typeCVV() {
   cy.wait(5000)
-  cy.get("iframe").then($iframe => {
-    const $body = $iframe.contents().find("body")
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Brand")
       .select("1")
     cy.wait(1000)
+  })
 
-    cy.wrap($body)
+  queryIframe($iframe => {
+    cy.wrap(getIframeBody($iframe))
       .find("#creditCardpayment-card-0Code")
       .type("066")
     cy.wait(1000)
