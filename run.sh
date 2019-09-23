d87beceed54cd068a132cba9ff172fad7eb4b9e1
nandoacoelho <nandoacoelho@gmail.com>
2019-09-22 23:54:27 -0300

    Remove unused files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function run {
-    node ./src/index.js
-    echo "Waiting 1 min..."
-    sleep 60
-}
-
-while [ 1 ]; do run; test $? -gt 128 && break; done
--- utils/testWrapper.js
+++
@@ -1,7 +0,0 @@
-import { ACCOUNT_NAMES } from "./constants";
-
-export function testWrapper(callback) {
-  Object.keys(ACCOUNT_NAMES).forEach(account => {
-    callback(account);
-  });
-}
