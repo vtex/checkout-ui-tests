9d6c50a8ad2e32c50e0f7c696e7d7f2f03a0a423
nandoacoelho <nandoacoelho@gmail.com>
2019-09-26 12:12:52 -0300

    Update sending videos to s3 removing lambda and adding credentials from aws cognito

--- src/monitoring/s3.js
+++ src/monitoring/s3.js
@@ -1,25 +1,37 @@
 const fs = require("fs")
 const AWS = require("aws-sdk")
-const s3 = new AWS.S3()
 const ACL = "public-read"
 const BUCKET = "vtex-id-hc"
 const KEY_PREFIX = "healthcheck/tests/"
+const REGION = 'us-east-1'
 const cwd = process.cwd()
+AWS.config.update({
+  region: REGION,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: process.env.HORUS_COGNITO_CREDENTIALS,
+  }),
+})
+const s3 = new AWS.S3()
 
 const uploadFile = async (src, dst, contentType = null) => {
   const key = KEY_PREFIX + dst
-  await s3
-    .putObject({
-      ACL,
-      Bucket: BUCKET,
-      Body: fs.readFileSync(src),
-      Key: key,
-      ContentType: contentType,
-    })
-    .promise()
-  return {
-    url: `https://${BUCKET}.s3.amazonaws.com/${key}`,
-    signed: s3.getSignedUrl("getObject", { Bucket: BUCKET, Key: key }),
+
+  try {
+    await s3
+      .putObject({
+        ACL,
+        Bucket: BUCKET,
+        Body: fs.readFileSync(src),
+        Key: key,
+        ContentType: contentType,
+      })
+      .promise()
+    return {
+      url: `https://${BUCKET}.s3.amazonaws.com/${key}`,
+      signed: s3.getSignedUrl("getObject", { Bucket: BUCKET, Key: key }),
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
