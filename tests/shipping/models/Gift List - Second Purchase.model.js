df32690b91932de4c937e22b7d11f14ea51f73df
nandoacoelho <nandoacoelho@gmail.com>
2019-10-21 15:09:30 -0300

    Add recurring purchase gift list scenario

--- tests/shipping/models/Gift List - Second Purchase.model.js
+++ tests/shipping/models/Gift List - Second Purchase.model.js
@@ -1,30 +1,30 @@
 import { setup, visitAndClearCookies } from "../../../utils"
 import {
   fillEmail,
-  getRandomEmail,
-  fillProfile,
+  getSecondPurchaseEmail,
+  confirmSecondPurchase,
 } from "../../../utils/profile-actions"
 import { payWithBoleto, completePurchase } from "../../../utils/payment-actions"
 
 export default function test(account) {
-  describe(`Gift List - ${account}`, () => {
+  describe(`Gift List - Second Purchase - ${account}`, () => {
     before(() => {
       visitAndClearCookies(account)
     })
 
     it("gift list with delivery", () => {
-      const email = getRandomEmail()
+      const email = getSecondPurchaseEmail()
 
       setup({ mobile: false, isGiftList: true, skus: ["31"], account })
       fillEmail(email)
-      fillProfile()
+      confirmSecondPurchase()
       payWithBoleto()
       completePurchase()
 
       cy.url({ timeout: 120000 }).should("contain", "/orderPlaced")
       cy.wait(2000)
       cy.contains(email).should("be.visible")
-      cy.contains("Fernando Coelho").should("be.visible")
+      cy.contains("aut* aut*").should("be.visible")
       cy.contains("Teste Endereço").should("be.visible")
     })
   })
