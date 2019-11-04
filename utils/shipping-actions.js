9b09901199de7d97397093b89f1d8f6bfc695a1f
nandoacoelho <nandoacoelho@gmail.com>
2019-11-04 19:07:46 -0300

    Update vtexgame1 tests with editing receiverName

--- utils/shipping-actions.js
+++ utils/shipping-actions.js
@@ -1,30 +1,30 @@
-import { ACCOUNT_NAMES } from "./constants"
+import { ACCOUNT_NAMES } from './constants'
 
 function fillPostalCodeOmnishipping() {
   cy.wait(1000)
-  cy.get("#ship-postalCode").type("22071060")
+  cy.get('#ship-postalCode').type('22071060')
 }
 
 function fillGeolocationOmnishipping() {
   cy.wait(2000)
 
-  cy.get("#ship-addressQuery").type("Rua Saint Roman 12")
+  cy.get('#ship-addressQuery').type('Rua Saint Roman 12')
 
-  cy.get(".pac-item")
+  cy.get('.pac-item')
     .first()
-    .trigger("mouseover")
+    .trigger('mouseover')
 
-  cy.get(".pac-item")
+  cy.get('.pac-item')
     .first()
     .click()
 
   cy.wait(2000)
-  cy.contains("Rua Saint Roman 12")
+  cy.contains('Rua Saint Roman 12')
 }
 
 function fillAddressInformation() {
   cy.wait(2000)
-  cy.get("#ship-number").type("12")
+  cy.get('#ship-number').type('12')
 }
 
 function shouldActivateDatePicker(account) {
@@ -38,7 +38,7 @@ function shouldActivateDatePicker(account) {
 
 export function unavailableDeliveryGoToPickup() {
   cy.wait(1000)
-  cy.get(".vtex-omnishipping-1-x-pickupButton").click({ force: true })
+  cy.get('.vtex-omnishipping-1-x-pickupButton').click({ force: true })
 }
 
 export function fillShippingInformation(account) {
@@ -52,46 +52,46 @@ export function fillShippingInformation(account) {
 
 export function fillRemainingInfo() {
   cy.wait(1000)
-  cy.get(".vtex-omnishipping-1-x-btnDelivery").click({ force: true })
+  cy.get('.vtex-omnishipping-1-x-btnDelivery').click({ force: true })
 }
 
 export function goToPayment() {
   cy.wait(1000)
-  cy.get(".btn-go-to-payment").click({ force: true })
+  cy.get('.btn-go-to-payment').click({ force: true })
 }
 
 export function chooseDelivery() {
-  cy.get("#shipping-option-delivery").click()
+  cy.get('#shipping-option-delivery').click()
 }
 
 export function choosePickup() {
   cy.wait(1000)
-  cy.get("#shipping-option-pickup-in-point").click()
+  cy.get('#shipping-option-pickup-in-point').click()
 }
 
 export function fillShippingPreviewDelivery() {
   cy.wait(1000)
-  cy.get("button#shipping-calculate-link").click()
+  cy.get('button#shipping-calculate-link').click()
 
-  cy.get("#ship-postalCode").type("22071060")
+  cy.get('#ship-postalCode').type('22071060')
 
-  cy.get("#cart-shipping-calculate").click()
+  cy.get('#cart-shipping-calculate').click()
 }
 
 export function choosePickupShippingPreview() {
-  cy.get(".srp-toggle__pickup").click()
+  cy.get('.srp-toggle__pickup').click()
 
   cy.wait(1000)
-  cy.contains("Retirar 1 item").should("be.visible")
-  cy.contains("Loja em Copacabana no Rio de Janeiro").should("be.visible")
+  cy.contains('Retirar 1 item').should('be.visible')
+  cy.contains('Loja em Copacabana no Rio de Janeiro').should('be.visible')
 }
 
 export function chooseDeliveryShippingPreview() {
-  cy.get(".srp-toggle__delivery").click()
+  cy.get('.srp-toggle__delivery').click()
 
   cy.wait(1000)
-  cy.contains("Receber 1 item").should("be.visible")
-  cy.contains("22071-060").should("be.visible")
+  cy.contains('Receber 1 item').should('be.visible')
+  cy.contains('22071-060').should('be.visible')
 
   cy.wait(1000)
 }
@@ -99,15 +99,15 @@ export function chooseDeliveryShippingPreview() {
 export function chooseDeliveryDate(account) {
   if (shouldActivateDatePicker(account)) {
     cy.wait(4000)
-    cy.get("#scheduled-delivery-delivery").click()
+    cy.get('#scheduled-delivery-delivery').click()
   }
   cy.wait(3000)
   if (account === ACCOUNT_NAMES.NO_LEAN) {
-    cy.get("#scheduled-delivery-choose-agendada-top").click({ force: true })
+    cy.get('#scheduled-delivery-choose-agendada-top').click({ force: true })
   } else {
-    cy.get("#scheduled-delivery-choose-agendada").click({ force: true })
+    cy.get('#scheduled-delivery-choose-agendada').click({ force: true })
   }
-  cy.get(".react-datepicker__day--keyboard-selected").click({ force: true })
+  cy.get('.react-datepicker__day--keyboard-selected').click({ force: true })
 }
 
 export function choosePickupDate(account) {
@@ -116,8 +116,10 @@ export function choosePickupDate(account) {
     cy.get('[id="scheduled-delivery-choose-pickup-(141125d)"]').click()
   }
   cy.wait(3000)
-  cy.get('[id="scheduled-delivery-choose-pickup-(141125d)"]').click({ force: true })
-  cy.get(".react-datepicker__day--keyboard-selected").click({ force: true })
+  cy.get('[id="scheduled-delivery-choose-pickup-(141125d)"]').click({
+    force: true,
+  })
+  cy.get('.react-datepicker__day--keyboard-selected').click({ force: true })
 }
 
 export function fillPickupAddress(account) {
@@ -128,21 +130,33 @@ export function fillPickupAddress(account) {
       ACCOUNT_NAMES.INVOICE,
     ].some(localAccount => localAccount === account)
   ) {
-    cy.get("#find-pickups-manualy-button-denied").click()
-    cy.get("#pkpmodal-search #ship-postalCode").type("22071060")
+    cy.get('#find-pickups-manualy-button-denied').click()
+    cy.get('#pkpmodal-search #ship-postalCode').type('22071060')
   } else {
-    cy.get("#find-pickups-manualy-button").click()
-    cy.get("#pkpmodal-search input").type("Praia de Botafogo, 300")
+    cy.get('#find-pickups-manualy-button').click()
+    cy.get('#pkpmodal-search input').type('Praia de Botafogo, 300')
 
-    cy.get(".pac-item")
+    cy.get('.pac-item')
       .first()
-      .trigger("mouseover")
+      .trigger('mouseover')
 
-    cy.get(".pac-item")
+    cy.get('.pac-item')
       .first()
       .click()
   }
-  cy.get(".pkpmodal-points-list .pkpmodal-pickup-point-main").click()
+  cy.get('.pkpmodal-points-list .pkpmodal-pickup-point-main').click()
 
-  cy.get(".pkpmodal-details-confirm-btn").click()
+  cy.get('.pkpmodal-details-confirm-btn').click()
+
+  if (account === ACCOUNT_NAMES.DEFAULT) {
+    editReceiverName()
+  }
+}
+
+export function editReceiverName() {
+  cy.get('.shp-pickup-receiver__btn').click()
+  cy.get('#pickup-receiver')
+    .clear()
+    .should('have.value', '')
+    .type('Test name')
 }
