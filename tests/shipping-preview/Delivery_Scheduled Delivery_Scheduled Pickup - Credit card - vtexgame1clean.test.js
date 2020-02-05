94a73955fd3c01eb0cfe06622f57d167ac53f6b5
Jefferson <jefiim.ad@gmail.com>
2020-02-05 17:21:10 -0300

    Add shipping preview Delivery, Scheduled delivery, Scheduled pickup tests

---
+++ tests/shipping-preview/Delivery_Scheduled Delivery_Scheduled Pickup - Credit card - vtexgame1clean.test.js
@@ -0,0 +1,5 @@
+
+  import test from "./models/Delivery_Scheduled Delivery_Scheduled Pickup - Credit card.model.js"
+
+  test("vtexgame1clean")
+  
---
+++ tests/shipping-preview/models/Delivery_Scheduled Delivery_Scheduled Pickup - Credit card.model.js
@@ -0,0 +1,70 @@
+import { setup, visitAndClearCookies } from "../../../utils"
+import { ACCOUNT_NAMES } from "../../../utils/constants"
+import { goToInvoiceAddress } from "../../../utils/invoice-actions"
+import {
+  completePurchase,
+  payWithCreditCard,
+} from "../../../utils/payment-actions"
+import {
+  fillEmail,
+  fillProfile,
+  getRandomEmail,
+} from "../../../utils/profile-actions"
+import {
+  chooseDeliveryDate,
+  choosePickupDate,
+  fillRemainingInfo,
+  fillShippingInformation,
+  goToPayment,
+  fillShippingPreviewPickupAddress,
+  goToShippingPreviewPickup,
+} from "../../../utils/shipping-actions"
+import { SKUS } from "../../../utils/constants"
+
+export default function test(account) {
+  describe(`Delivery + Scheduled Delivery + Scheduled Pickup - Credit card - ${account}`, () => {
+    before(() => {
+      visitAndClearCookies(account)
+    })
+
+    it("delivery with scheduled delivery and scheduled pickup", () => {
+      const email = getRandomEmail()
+      setup({
+        skus: [
+          SKUS.DELIVERY_CUSTOMIZATION_ATTACHMENT,
+          SKUS.SCHEDULED_DELIVERY,
+          SKUS.SCHEDULED_PICKUP,
+        ],
+        account,
+      })
+
+      goToShippingPreviewPickup()
+      fillShippingPreviewPickupAddress(account)
+
+      fillEmail(email)
+      fillProfile()
+      fillRemainingInfo()
+      fillShippingInformation(account)
+      choosePickupDate({ account })
+      chooseDeliveryDate({
+        account,
+        shouldActivate: false,
+      })
+      goToInvoiceAddress(account)
+      goToPayment()
+      payWithCreditCard()
+      completePurchase()
+
+      cy.url({ timeout: 120000 }).should("contain", "/orderPlaced")
+      cy.wait(2000)
+      cy.contains(email).should("be.visible")
+      cy.contains("Fernando Coelho").should("be.visible")
+      cy.contains("5521999999999").should("be.visible")
+      cy.contains("Receber").should("be.visible")
+      cy.contains("Rua Saint Roman 12").should("be.visible")
+      cy.contains("Copacabana").should("be.visible")
+      cy.contains("Agendada").should("be.visible")
+      cy.contains("Retirar").should("be.visible")
+    })
+  })
+}
