ab7532dd7fd3efaa4e2b919ae6e75fbfef7889d7
Jefferson <jefiim.ad@gmail.com>
2020-02-05 17:17:51 -0300

    Add shipping preview Pickup and Scheduled delivery tests

---
+++ tests/shipping-preview/Pickup_Scheduled Delivery - Credit card - vtexgame1clean.test.js
@@ -0,0 +1,5 @@
+
+  import test from "./models/Pickup_Scheduled Delivery - Credit card.model.js"
+
+  test("vtexgame1clean")
+  
---
+++ tests/shipping-preview/models/Pickup_Scheduled Delivery - Credit card.model.js
@@ -0,0 +1,62 @@
+import { setup, visitAndClearCookies } from "../../../utils"
+import {
+  fillEmail,
+  getRandomEmail,
+  fillProfile,
+  login,
+} from "../../../utils/profile-actions"
+import {
+  goToPayment,
+  fillRemainingInfo,
+  fillShippingInformation,
+  chooseDeliveryDate,
+  fillShippingPreviewPickupAddress,
+  goToShippingPreviewPickup,
+} from "../../../utils/shipping-actions"
+import {
+  completePurchase,
+  payWithCreditCard,
+} from "../../../utils/payment-actions"
+import { goToInvoiceAddress } from "../../../utils/invoice-actions"
+import { SKUS } from "../../../utils/constants"
+
+export default function test(account) {
+  describe(`Pickup + Scheduled Delivery - Credit card - ${account}`, () => {
+    before(() => {
+      visitAndClearCookies(account)
+    })
+
+    it("with only pickup", () => {
+      const email = getRandomEmail()
+
+      setup({
+        skus: [SKUS.PICKUP_1_SLA, SKUS.SCHEDULED_DELIVERY],
+        account,
+      })
+      goToShippingPreviewPickup()
+      fillShippingPreviewPickupAddress(account)
+      fillEmail(email)
+      fillProfile()
+      fillRemainingInfo()
+      fillShippingInformation(account)
+      chooseDeliveryDate({ account })
+      goToInvoiceAddress(account)
+      goToPayment()
+      payWithCreditCard()
+      completePurchase()
+
+      cy.url({ timeout: 120000 }).should("contain", "/orderPlaced")
+      cy.wait(2000)
+      cy.contains(email).should("be.visible")
+      cy.contains("Fernando Coelho").should("be.visible")
+      cy.contains("5521999999999").should("be.visible")
+      cy.contains("Retirar").should("be.visible")
+      cy.contains("Loja em Copacabana no Rio de Janeiro").should("be.visible")
+      cy.contains("Rua General Azevedo Pimentel 5").should("be.visible")
+      cy.contains("Copacabana").should("be.visible")
+      cy.contains("Agendada").should("be.visible")
+      cy.contains("Rua Saint Roman 12").should("be.visible")
+      cy.contains("Copacabana").should("be.visible")
+    })
+  })
+}
