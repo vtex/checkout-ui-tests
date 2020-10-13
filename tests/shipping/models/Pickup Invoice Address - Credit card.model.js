e737b008a3b9754b01d51e5ee06039a19439fde6
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-10-13 17:54:51 -0300

    Add tests for invoice address validation

---
+++ tests/shipping/models/Pickup Invoice Address - Credit card.model.js
@@ -0,0 +1,100 @@
+import { setup, visitAndClearCookies } from '../../../utils'
+import {
+  fillEmail,
+  getRandomEmail,
+  fillProfile,
+} from '../../../utils/profile-actions'
+import { fillPickupAddress, goToPayment } from '../../../utils/shipping-actions'
+import {
+  completePurchase,
+  payWithCreditCard,
+} from '../../../utils/payment-actions'
+import {
+  goToInvoiceAddress,
+  fillInvoiceAddress,
+} from '../../../utils/invoice-actions'
+import { ACCOUNT_NAMES, SKUS } from '../../../utils/constants'
+
+export default function test(account) {
+  describe(`Pickup Invoice Address - Credit card - ${account}`, () => {
+    before(() => {
+      visitAndClearCookies(account)
+    })
+
+    it('should proceed with valid address only', () => {
+      const email = getRandomEmail()
+
+      setup({ skus: [SKUS.PICKUP_1_SLA], account })
+      fillEmail(email)
+      fillProfile()
+      fillPickupAddress(account)
+      goToInvoiceAddress(account)
+      fillInvoiceAddress(account)
+
+      cy.waitAndGet('#ship-number', 1000)
+        .last()
+        .clear()
+        .blur()
+      cy.get('#btn-go-to-payment').should('not.exist')
+      cy.get('.ship-number .error')
+        .should('exist')
+        .contains('Campo obrigatório.')
+      cy.get('#ship-number').type('12')
+
+      goToPayment()
+      payWithCreditCard({ withAddress: account !== ACCOUNT_NAMES.INVOICE })
+      completePurchase()
+
+      cy.url({ timeout: 120000 }).should('contain', '/orderPlaced')
+      cy.wait(2000)
+      cy.contains(email).should('be.visible')
+      cy.contains('Fernando Coelho').should('be.visible')
+      cy.contains('5521999999999').should('be.visible')
+      cy.contains('Cartão de crédito').should('be.visible')
+      cy.contains('final 8936').should('be.visible')
+      cy.contains('Retirar').should('be.visible')
+      cy.contains('Rua General Azevedo Pimentel 5').should('be.visible')
+      cy.contains('Copacabana').should('be.visible')
+    })
+
+    it('should invalidate go to payment button even after user inserted correct address once', () => {
+      const email = getRandomEmail()
+
+      setup({ skus: [SKUS.PICKUP_1_SLA], account })
+      fillEmail(email)
+      fillProfile()
+      fillPickupAddress(account)
+      goToInvoiceAddress(account)
+      fillInvoiceAddress(account)
+      goToPayment()
+
+      cy.get('#edit-shipping-data').click()
+
+      cy.waitAndGet('#ship-number', 1000)
+        .last()
+        .clear()
+        .blur()
+      cy.get('.ship-number .error')
+        .should('exist')
+        .contains('Campo obrigatório.')
+      cy.get('#btn-go-to-payment').should('not.exist')
+      cy.get('#ship-number').type('12')
+
+      goToPayment()
+
+      payWithCreditCard({ withAddress: account !== ACCOUNT_NAMES.INVOICE })
+      completePurchase()
+
+      cy.url({ timeout: 120000 }).should('contain', '/orderPlaced')
+      cy.wait(2000)
+      cy.contains(email).should('be.visible')
+      cy.contains('Fernando Coelho').should('be.visible')
+      cy.contains('5521999999999').should('be.visible')
+      cy.contains('Cartão de crédito').should('be.visible')
+      cy.contains('final 8936').should('be.visible')
+      cy.contains('Retirar').should('be.visible')
+      cy.contains('Rua General Azevedo Pimentel 5').should('be.visible')
+      cy.contains('Copacabana').should('be.visible')
+    })
+  })
+}
