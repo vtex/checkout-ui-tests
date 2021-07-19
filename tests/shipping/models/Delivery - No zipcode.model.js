863a4f8bd3fa111815f4d324e3fd8c94ef9a5942
SilviaFonseca <silviasfon@gmail.com>
2021-07-19 14:29:06 -0300

    pull request corrections

--- tests/shipping/models/Delivery - No zipcode.model.js
+++ tests/shipping/models/Delivery - No zipcode.model.js
@@ -17,7 +17,7 @@ import {
 import { SKUS } from '../../../utils/constants'
 
 export default function test(account) {
-  describe(`Delivery - Credit Card - ${account}`, () => {
+  describe(`Delivery - No zipcode - ${account}`, () => {
     before(() => {
       visitAndClearCookies(account)
     })
