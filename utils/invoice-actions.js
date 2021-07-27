18a1fad8d71e327be95df565f99515ff0a76b741
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-07-27 15:57:30 -0300

    Add test for invoice address form without zipcode

--- utils/invoice-actions.js
+++ utils/invoice-actions.js
@@ -1,40 +1,62 @@
 import { ACCOUNT_NAMES } from './constants'
 import { waitLoad } from '.'
 
+const INVOICE_ACCOUNTS = [
+  ACCOUNT_NAMES.INVOICE,
+  ACCOUNT_NAMES.GEOLOCATION_INVOICE,
+]
+
 export function goToInvoiceAddress(account) {
-  if (account !== ACCOUNT_NAMES.INVOICE) {
+  if (!INVOICE_ACCOUNTS.includes(account)) {
     return
   }
 
   waitLoad()
   cy.waitAndGet('.vtex-omnishipping-1-x-btnDelivery', 1000).click()
-  cy.focused().blur()
+
+  if (account === ACCOUNT_NAMES.INVOICE) {
+    cy.focused().blur()
+  }
 }
 
 export function fillInvoiceAddress(account) {
-  if (account !== ACCOUNT_NAMES.INVOICE) {
+  if (!INVOICE_ACCOUNTS.includes(account)) {
     return
   }
 
-  cy.waitAndGet(
-    '.vtex-omnishipping-1-x-addressFormPart1 #ship-postalCode',
-    1000
-  )
-    .last()
-    .clear()
-    .type('22071060')
-    .should('have.value', '22071-060')
+  if (account === ACCOUNT_NAMES.INVOICE) {
+    cy.waitAndGet(
+      '.vtex-omnishipping-1-x-addressFormPart1 #ship-postalCode',
+      1000
+    )
+      .last()
+      .clear()
+      .type('22071060')
+      .should('have.value', '22071-060')
+  } else if (account === ACCOUNT_NAMES.GEOLOCATION_INVOICE) {
+    cy.waitAndGet('#ship-addressQuery', 1000).type('Rua Saint Roman')
+
+    cy.get('.pac-item')
+      .first()
+      .trigger('mouseover')
+
+    cy.get('.pac-item')
+      .first()
+      .click()
+
+    cy.contains('Rua Saint Roman')
+  }
 
   cy.wait('@checkoutRequest')
 
-  cy.waitAndGet('.vtex-omnishipping-1-x-teste #ship-number', 1000)
+  cy.waitAndGet('#ship-number', 1000)
     .last()
     .clear()
     .type('12')
 }
 
 export function invalidateInvoiceAddress(account) {
-  if (account !== ACCOUNT_NAMES.INVOICE) {
+  if (!INVOICE_ACCOUNTS.includes(account)) {
     return
   }
 
