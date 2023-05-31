17f1f9100e9625f4de097ec2d53c91a621e6e860
Marcos Oliveira <marcosoliveira@vtex.com.br>
2023-05-31 15:34:24 -0300

    migrating config and setup for cypress@10

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  blockHosts: ['www.googletagmanager.com'],
+  video: true,
+  projectId: 'kobqo4',
+  pageLoadTimeout: 180000,
+  defaultCommandTimeout: 15000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  e2e: {
+    setupNodeEvents(on) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name !== 'chrome') {
+          return
+        }
+
+        launchOptions.args.push('--disable-site-isolation-trials')
+      })
+    },
+    specPattern: 'tests/**/*.test.{js,ts}',
+  },
+})
