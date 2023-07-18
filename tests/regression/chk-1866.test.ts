859cf3799452c974e07ebb9a09897cfe067914d0
Everton Strack <everton.strack@vtex.com.br>
2023-07-18 12:08:02 -0300

    Add regression test for CHK-1866

---
+++ tests/regression/chk-1866.test.ts
@@ -0,0 +1,43 @@
+import { visitAndClearCookies, setup } from '../../utils'
+import { Accounts, SKUs } from '../../utils/constants'
+import { calculateShippingPreview } from '../../utils/shipping-actions'
+
+describe('CHK-1866', () => {
+  describe(`${Accounts.DEFAULT}`, () => {
+    beforeEach(() => {
+      visitAndClearCookies(Accounts.DEFAULT)
+    })
+
+    it('should the price shown in the shipping preview will be the same as shown in the cart summary', () => {
+      setup({
+        skus: [SKUs.ONLY_PICKUP_2_SLA_RJ, '75530389'],
+        salesChannel: '1',
+        account: Accounts.DEFAULT,
+      })
+
+      calculateShippingPreview()
+
+      cy.get('#ship-postalCode').type('22250-040')
+      cy.wait(3000)
+
+      // .srp-summary-result .monetary
+
+      cy.get('.srp-shipping-current-single__price').then(
+        ($shippingPreviewPrice) => {
+          const shippingPreviewPrice = $shippingPreviewPrice.text()
+
+          cy.wait(3000)
+
+          cy.get(
+            '.summary-template-holder .summary-totalizers .srp-summary-result .monetary'
+          ).then(($field) => {
+            const price = $field.first().text()
+            const priceFormated = price.replace('A partir de ', '')
+
+            expect(shippingPreviewPrice).equal(priceFormated)
+          })
+        }
+      )
+    })
+  })
+})
